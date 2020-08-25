b284534e3062329dfeb91c3939da4a009ff5f76b
Marcin Stefanowicz <19smarcins98@gmail.com>
2020-08-25 13:50:26 +0200

    starting coding in js

--- Session.js
+++ Session.js
@@ -1,24 +1,32 @@
 class Session {
     constructor() {
+        this.logInPanel = document.querySelector('div.log-in-panel');
         this.logInInputLogInPanel = document.querySelector('div.log-in-panel input.login');
         this.passwordInputLogInPanel = document.querySelector('div.log-in-panel input.password');
         this.logInButtonLogInPanel = document.querySelector('div.log-in-panel button.logIn');
         this.signUpButtonLogInPanel = document.querySelector('div.log-in-panel button.signUp');
+        this.signUpPanel = document.querySelector('div.sign-up-panel');
         this.logInInputSignUpPanel = document.querySelector('div.sign-up-panel input.login');
         this.passwordInputSignUpPanel = document.querySelector('div.sign-up-panel input.password');
         this.confirmPasswordInputSignUpPanel = document.querySelector('div.sign-up-panel input.confirm-password');
         this.logInButtonSignUpPanel = document.querySelector('div.sign-up-panel button.cancel');
         this.signUpButtonSignUpPanel = document.querySelector('div.sign-up-panel button.signUp');
 
+        console.log(this.logInPanel);
         console.log(this.logInInputLogInPanel);
         console.log(this.passwordInputLogInPanel);
         console.log(this.logInButtonLogInPanel);
         console.log(this.signUpButtonLogInPanel);
 
+        console.log(this.signUpPanel);
         console.log(this.logInInputSignUpPanel);
         console.log(this.passwordInputSignUpPanel);
         console.log(this.confirmPasswordInputSignUpPanel);
         console.log(this.logInButtonSignUpPanel);
         console.log(this.signUpButtonSignUpPanel);
+
+        this.signUpButtonLogInPanel.addEventListener('click', () => {
+            Slider.changeSlide();
+        });
     }
 }
---
+++ Slider.js
@@ -0,0 +1,5 @@
+class Slider {
+    static changeSlide() {
+        console.log('działam!');
+    }
+}
